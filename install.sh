41181ce72eeaaccdefba0493f6ac70d097d1182c
oprextv <oprextv69@gmail.com>
2026-01-04 09:21:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # kodi repo add
 apt install software-properties-common -y
-add-apt-repository ppa:team-xbmc/ppa -y
+add-apt-repository ppa:ubuntuhandbook1/kodi
 
 # update and upgrade
 apt update -y && apt upgrade -y
